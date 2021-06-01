723fb0afea5f2826c53a92edf9f0f5e149d65a52
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-06-01 13:58:58 +0530

    hibernate : imp link

--- src/main/java/com/ani/orm/ormdetails/OrmDetailsApplication.java
+++ src/main/java/com/ani/orm/ormdetails/OrmDetailsApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrmDetailsApplication {
-
 	//https://javabydeveloper.com/hibernate-5-example-with-different-configurations/
 	public static void main(String[] args) {
 		SpringApplication.run(OrmDetailsApplication.class, args);
 	}
-
 }
